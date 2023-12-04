b2e4d0773db691f56a2bddf3c81d32305aacdc82
spyrosbenekis <sdi2300132@di.uoa.gr>
2023-12-04 17:51:57 +0200

    final newton

--- newton/src/newton.c
+++ newton/src/newton.c
@@ -2,50 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-void newton(double xn);
-double f(double xn);
-double factor(double xn);
-
 int count = 0;
 double arr[6];
 
-int main(int argc, char **argv) {
-    
-    if (argc != 8) {
-        printf("Program needs to be called as `./newton a_0 a_1 a_2 a_3 a_4 a_5 x_0`\n");
-        return 1;
-    }
-
-    for (int i = 1; i <= 6; i++)
-        arr[i-1] = strtod(argv[i], NULL);
-
-    double x_0 = strtod(argv[7], NULL);
-
-    newton(x_0);
-
-    return 0;
-}
-
-void newton(double xn)
-{
-    double xn1;
-    double fac = factor(xn);
-
-    if (fac == 0.0)
-        printf("nan\n");
-    else if (count > 1000)
-        printf("incomplete\n");
-    else{
-        count++;
-        xn1 = xn - f(xn) / fac;
-        if (fabs(xn1 - xn) < 0.000001)
-            printf("%.2f\n", xn1);
-        else
-            newton(xn1);
-    }
-}
-
-double f(double xn)
+double f(double xn) //Υπολογισμός συνάρτησης στο σημείο
 {
     double num = 0;
 
@@ -56,17 +16,54 @@ double f(double xn)
     return num;
 }
 
-double factor(double xn)
+double factor(double xn) //Υπολογισμός παραγώγου στο σημείο
 {
     double fac=arr[1];
 
     for(int i=2;i<6;i++){
-        fac+=arr[i]*i*pow(xn,i-1);
+        fac+=arr[i]*i*pow(xn,i-1); 
     }
 
-    if (fabs(fac) < 0.0001) {
+    if (fabs(fac) < 0.0001) //Έλεγχος αν η παράγωγος συγκλίνει στο μηδέν
         return 0.0;
-    }
     else 
         return fac;
+}
+
+void newton(double xn) //Αναδρομή
+{
+    double xn1;
+    double fac = factor(xn);
+
+    if (fac == 0.0) //Έλεγχος αν ο αλγόριθμος αποκλίνει (διαίρεση με 0)
+        printf("nan\n");
+    else if (count > 1000) //Έλεγχος αν έχουν γίνει περισσότερες από 1000 επαναλήψεις
+        printf("incomplete\n");
+    else{
+        count++;
+        xn1 = xn - f(xn) / fac;
+        if (fabs(xn1 - xn) < 0.000001) //Έλεγχος αν ο αλγόριθμος συγκλίνει και ισχύει  |xn+1 − xn| < 10^−6
+            printf("%.2f\n", xn1); //Εκτύπωση του αποτελέσματος 
+        else
+            newton(xn1); // Αφού δεν έχει βρεθεί ξανά καλεί την συνάρτηση
+    }
+}
+
+int main(int argc, char **argv) {
+    
+    //Έλεγχος ορισμάτων
+    if (argc != 8) {
+        printf("Program needs to be called as `./newton a_0 a_1 a_2 a_3 a_4 a_5 x_0`\n");
+        return 1;
+    }
+
+    //Αποθήκευση δεδομένων εισόδου
+    for (int i = 1; i <= 6; i++)
+        arr[i-1] = strtod(argv[i], NULL);
+
+    double x_0 = strtod(argv[7], NULL);
+
+    newton(x_0); //Κλήση αναδρομής
+
+    return 0;
 }
